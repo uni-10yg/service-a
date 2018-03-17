84786240e1f5572de4ed9d1547dfb4bb87e98214
ydu <>
2018-03-18 00:44:09 +0100

    ...

--- service.py
+++ service.py
@@ -14,10 +14,9 @@ channel = connection.channel()
 channel.queue_declare(queue='comm_queue')
 
 def callback(ch, method, properties, body):
-    logger.info(" [x] Received %r" % body)
     body_dict = json.loads(body.decode())
     db.insert(body_dict.get('hash'), body_dict.get('salt'))
-    logger.debug(db.selectAll())
+    logger.info(db.selectAll())
 
 channel.basic_consume(
     callback,
