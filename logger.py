00600f48b9510e16196e0de902f87c1c9e13ce32
ydu <>
2018-03-18 00:41:42 +0100

    ...

--- logger.py
+++ logger.py
@@ -1,7 +1,7 @@
 import logging
 
 root_logger = logging.getLogger()
-root_logger.setLevel(logging.info)
+root_logger.setLevel(logging.INFO)
 log_handler = logging.StreamHandler()
 log_handler.setFormatter(logging.Formatter(fmt='%(asctime)s: %(message)s', datefmt='%m.%d.%Y %I:%M:%S %p'))
 root_logger.addHandler(log_handler)
